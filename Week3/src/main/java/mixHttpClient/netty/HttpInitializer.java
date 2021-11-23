94775f166f5c155fe69dcd5482b930dc6930fca0
HeroHz <pesthjh@163.com>
2021-11-23 10:54:02 +0800

    Week3

---
+++ Week3/src/main/java/mixHttpClient/netty/HttpInitializer.java
@@ -0,0 +1,22 @@
+package mixHttpClient.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HttpInitializer extends ChannelInitializer<SocketChannel> {
+	private  String httpServer ;
+	public HttpInitializer(String httpServer){
+		this.httpServer = httpServer;
+	}
+	@Override
+	public void initChannel(SocketChannel ch) {
+		ChannelPipeline p = ch.pipeline();
+		p.addLast(new HttpServerCodec());
+		//p.addLast(new HttpServerExpectContinueHandler());
+		p.addLast(new HttpObjectAggregator(1024 * 1024));
+		p.addLast(new HttpHandler(httpServer));
+	}
+}
